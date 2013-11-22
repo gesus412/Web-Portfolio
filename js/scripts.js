31c4bd45ed36020b828b090af9d7e55f5e838731
Giovanni Leone <Giovanni@Giovannis-MacBook-Pro-2.local>
2013-11-21 20:37:53 -0500

    Done with Class tonight

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".gallery a").lightBox();
+	$("#gallery1 a").lightBox();
+	$("#gallery2 a").lightBox();
+	$("#gallery3 a").lightBox();
+	$("#gallery4 a").lightBox();
+	$("#gallery5 a").lightBox();
+	$("#gallery6 a").lightBox();
+});
